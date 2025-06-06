ed40c75f4255da9e264f0376c7b48a30295badfd
craven95 <cravendiot@gmail.com>
2025-06-06 14:44:16 +0400

    add lint check

---
+++ interface/eslint.config.cjs
@@ -0,0 +1,65 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require("@eslint/js");
+const path = require('path');
+const globals = require('globals');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended
+});
+
+module.exports = [
+  {
+    ignores: [
+      "**/node_modules/**",
+      ".next/**",
+      "dist/**",
+      "out/**",
+      "*.d.ts"
+    ]
+  },
+  ...compat.extends('plugin:react/recommended'),
+  ...compat.extends('plugin:react-hooks/recommended'),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "no-console": "warn",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/no-unescaped-entities": "off",
+    }
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    },
+    plugins: {
+      "@typescript-eslint": require('@typescript-eslint/eslint-plugin')
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { "argsIgnorePattern": "^_" }
+      ],
+      "@typescript-eslint/no-explicit-any": "warn"
+    }
+  }
+];
